a1d8d4f07b61c7b84580479c1a5e72a355846f5e
garvshr <garvsharma40@gmail.com>
2026-01-27 16:04:58 +0530

    [Garv] Add : Added class task

--- task 22-01/writable.js
+++ task 22-01/writable.js
@@ -5,13 +5,5 @@ const inputStream = path.resolve(__dirname, 'input.txt')
 const outputStream = path.resolve(__dirname, 'output.txt')
 
 const readStream = fs.createReadStream(inputStream)
-// const writeStream = fs.createWriteStream(outputStream)
+const writeStream = fs.createWriteStream(outputStream)
 
-// readStream.pipe(writeStream);
-
-// );
-
-readStream.on('data', (chunk) => {
-    console.log('--- New Chunk ---');
-    console.log(chunk.toString());
-});
